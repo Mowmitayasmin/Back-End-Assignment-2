3e6d1b4c6b6e9ffd6adfe6012f6b40029c153229
Mowmita Yasmin <meite@academin.rrc.ca>
2025-03-19 08:29:17 -0500

    Implement createLoan function to create loan in loan controller

--- src/api/v1/controllers/loan.ts
+++ src/api/v1/controllers/loan.ts
@@ -18,3 +18,28 @@ export const getAllLoans = async (
         next(error);
     }
 };
+
+export const createItem = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+): Promise<void> => {
+    try {
+        const data = {
+            ...req.body,
+            is_reviewed: 0,
+            is_approved: 0
+        }
+
+        const item: Loan = await loanService.createDocuments(data);
+
+      res.status(HTTP_STATUS.CREATED).json(
+            {
+                message: 'Item created susscssfully',
+                item: item
+            }
+        );
+    } catch (error) {
+        next(error);
+    }
+}
