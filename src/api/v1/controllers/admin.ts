8be75c5b0d22e0c267e3ab1ba325745528aa8b4b
Mowmita Yasmin <meite@academin.rrc.ca>
2025-03-19 07:24:24 -0500

    Implement admin controller to manage user roles through Firebase custom claims

---
+++ src/api/v1/controllers/admin.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from 'express'
+import { auth } from "../../../../config/firebaseConfig";
+import { HTTP_STATUS } from 'src/constants/httpConstants';
+
+ const customClaimes = async (req: Request, res: Response) => {
+    const { uid } = req.params;
+    const { role } = req.body;
+    try {
+        await auth.setCustomUserClaims(uid, { role });
+        res.status(HTTP_STATUS.OK).json({ message: `Role "${role}" assigned to user ${uid}` });
+    } catch (error) {
+        res.status(500).json({ error: "Failed to set custom claims" });
+    }
+}
+
+export default customClaimes
