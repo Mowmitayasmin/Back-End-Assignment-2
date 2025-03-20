0e5cdd740d0868219dceca3ad8e15818d298cbba
Mowmita Yasmin <meite@academin.rrc.ca>
2025-03-20 08:00:16 -0500

    Implement reviewLoan API endpoint with authentication & authorization

--- src/api/v1/routes/loan.ts
+++ src/api/v1/routes/loan.ts
@@ -1,7 +1,7 @@
 import express, { Router } from 'express';
 import { isAuthenticate } from '../middleware/authenticate';
 import { isAuthorize } from '../middleware/authorize';
-import { createLoan } from '../controllers/loan';
+import { createLoan, reviewLoan } from '../controllers/loan';
 
 const router: Router = express.Router();
 
@@ -9,5 +9,10 @@ router.post('/create', isAuthenticate, isAuthorize({
     hasRole:['user'],
     allowSameUser: true
 }), createLoan)
+
+router.put('/:id/review', isAuthenticate, isAuthorize({
+    hasRole:['officer', 'admin'],
+    allowSameUser: true
+}), reviewLoan)
 
 export default router
