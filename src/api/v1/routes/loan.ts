83ef5e807e96c294973fb01faa6472442f6f46f4
Mowmita Yasmin <meite@academin.rrc.ca>
2025-03-20 08:01:27 -0500

    Implement approveLoan API endpoint with authentication & authorization

--- src/api/v1/routes/loan.ts
+++ src/api/v1/routes/loan.ts
@@ -1,7 +1,7 @@
 import express, { Router } from 'express';
 import { isAuthenticate } from '../middleware/authenticate';
 import { isAuthorize } from '../middleware/authorize';
-import { createLoan, reviewLoan } from '../controllers/loan';
+import { approveLoan, createLoan, reviewLoan } from '../controllers/loan';
 
 const router: Router = express.Router();
 
@@ -14,5 +14,10 @@ router.put('/:id/review', isAuthenticate, isAuthorize({
     hasRole:['officer', 'admin'],
     allowSameUser: true
 }), reviewLoan)
+
+router.put('/:id/approve', isAuthenticate, isAuthorize({
+    hasRole:['manager', 'admin'],
+    allowSameUser: true
+}), approveLoan)
 
 export default router
